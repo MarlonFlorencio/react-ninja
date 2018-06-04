b29dabad2a2a825b4912a0060bd669d5fe9befaf
Marlon Florencio <marlonarfl@gmail.com>
2018-06-03 22:23:57 -0300

    M3#A55

--- todo-list/src/app.js
+++ todo-list/src/app.js
@@ -4,12 +4,21 @@ import React from 'react'
 import Form from 'components/form'
 import TodosList from 'components/todos-list'
 import Filter from 'components/filter'
+import SearchCep from 'components/search-cep'
+
+import 'milligram'
 
 const App = () => (
-  <div>
-    <Form />
-    <TodosList />
-    <Filter />
+  <div style={{ display: 'flex', justifyContent: 'space-around', padding: 30 }}>
+    <div>
+      <Form />
+      <TodosList />
+      <Filter />
+    </div>
+
+    <div>
+      <SearchCep />
+    </div>
   </div>
 )
 
